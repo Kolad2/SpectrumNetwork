39d74945c5009280e8c27273493b548d6fe20c84
Kolad2 <koladik@gmail.com>
2025-06-23 15:03:37 +0300

    update

--- network.py
+++ network.py
@@ -9,27 +9,35 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 
+from sklearn.model_selection import train_test_split
+
 
 def main():
     data_path = Path('data/data_ascii')
     data = np.load('data/data_cheby_numpy/cheby_5.npy')
     params = pd.read_csv('data/data_ascii/parameters.csv')
+    
+    train_idx, test_idx = train_test_split(np.arange(len(data)), test_size=0.3, random_state=42)
+    data_train = data[train_idx]
+    data_test = data[test_idx]
+    
+    params_train = params.iloc[train_idx].copy()
+    params_test = params.iloc[test_idx].copy()
+    
     transform = Transforms()
-    x = transform.get_input_network(data, params)
-    y = transform.get_output_network(params)
+    x_train = transform.get_input_network(data_train, params_train)
+    y_train = transform.get_output_network(params_train)
     input_size = 7
     output_size = 2
     learning_rate = 0.01
-    num_epochs = 100
+    num_epochs = 1000
     batch_size = 32
-    model = MLP(input_size, 10, output_size)
+    model = MLP(input_size, 3, output_size)
     
     # Функция потерь и оптимизатор
     criterion = nn.MSELoss()     # для регрессии
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     
-    x_train = x
-    y_train = y
     # Создание DataLoader
     dataset = TensorDataset(x_train, y_train)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
@@ -39,30 +47,38 @@ def main():
         for batch_x, batch_y in dataloader:
             outputs = model(batch_x)
             loss = criterion(outputs, batch_y)
-            
             # Обратный проход и оптимизация
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        
         if (epoch + 1) % 10 == 0:
             print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
     
-    test_x = x_train
+    test_x = transform.get_input_network(data_test, params_test)
     test_y = model(test_x)
     test_y = transform.output_network_to_numpy(test_y)
     
-    params["test_Mo"] = test_y[:, 0]
-    params["test_Es"] = test_y[:, 1]
+    params_test["test_Mo"] = test_y[:, 0]
+    params_test["test_Es"] = test_y[:, 1]
     #params.to_csv('out/new_params.csv', sep=';', index=False)
     
-    
-    plt.plot(np.log(params["Mo"]), np.log(params["test_Mo"]), ".")
-    plt.plot(np.log(params["Es"]), np.log(params["test_Es"]), ".")
+    fig = plt.figure(figsize=(9, 5))
+    axs = [fig.add_subplot(1, 2, 1),
+           fig.add_subplot(1, 2, 2)]
+    axs[0].plot(params_test["Mo"], params_test["test_Mo"], ".")
+    axs[0].set_xscale('log')
+    axs[0].set_yscale('log')
+    axs[0].set_aspect('equal')
+    axs[0].set_xlabel("Истинные значения, $M_o$")
+    axs[0].set_ylabel("Модельные значения, $M_o^*$")
+    axs[1].plot(params_test["Es"], params_test["test_Es"], ".")
+    axs[1].set_xscale('log')
+    axs[1].set_yscale('log')
+    axs[1].set_aspect('equal')
+    axs[1].set_xlabel("Истинные значения, $E_s$")
+    axs[1].set_ylabel("Модельные значения, $E_s^*$")
     plt.show()
-    #print(params[["Mo", "Es", "test_Mo", "test_Es"]])
     
-
 
 class MLP(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
